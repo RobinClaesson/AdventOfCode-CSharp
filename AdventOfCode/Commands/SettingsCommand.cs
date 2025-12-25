12cee2b9fade84aacf68635df4e2bb4727396c0d
Robin Claesson <57729281+RobinClaesson@users.noreply.github.com>
2025-12-25 17:43:33 +0100

    Move SettingsCommand subcommand actions to methods

--- AdventOfCode/Commands/SettingsCommand.cs
+++ AdventOfCode/Commands/SettingsCommand.cs
@@ -8,59 +8,63 @@ public partial class SettingsCommand : Command
 {
     private static readonly List<PropertyInfo> SettingsProperties = typeof(Settings).GetProperties().ToList();
 
+    private static readonly List<Option<string>> SetOptions = SettingsProperties.Select(property =>
+            new Option<string>($"--{PascalToKebabCase(property.Name)}")
+            {
+                HelpName = property.Name,
+            })
+        .ToList();
+
     public SettingsCommand() : base("settings", "Manage Advent of Code settings")
     {
         var listCommand = new Command("list", "Lists all settings");
-        listCommand.SetAction(_ =>
-        {
-            var settings = Settings.LoadSettingsFile();
-            Console.WriteLine("Settings:");
-            ListSettings(settings);
-        });
+        listCommand.SetAction(_ => ListOptionsAction());
 
         var setCommand = new Command("set", "Sets the given settings");
-        var setOptions = SettingsProperties.Select(property =>
-                new Option<string>($"--{PascalToKebabCase(property.Name)}")
-                {
-                    HelpName = property.Name,
-                })
-            .ToList();
-        setOptions.ForEach(setCommand.Add);
-
-        setCommand.SetAction(parseResult =>
-        {
-            var optionValues = setOptions.ToDictionary(
-                option => option.HelpName,
-                parseResult.GetValue
-            );
-
-            if (optionValues.Values.All(string.IsNullOrWhiteSpace))
-            {
-                Console.WriteLine("No new settings provided. Too see options, run: ");
-                Console.WriteLine($"{AppDomain.CurrentDomain.FriendlyName} {Name} {setCommand.Name} -h");
-                return;
-            }
-
-            var currentSettings = Settings.LoadSettingsFile();
-            SettingsProperties.ForEach(property =>
-            {
-                var value = optionValues[property.Name];
-                if (!string.IsNullOrWhiteSpace(value))
-                    property.SetValue(currentSettings, value);
-            });
-            currentSettings.Save();
-
-            Console.WriteLine("Updated settings:");
-            ListSettings(currentSettings);
-        });
-
-
+        SetOptions.ForEach(setCommand.Add);
+        setCommand.SetAction(SetOptionsAction);
+        
         Add(listCommand);
         Add(setCommand);
     }
 
+    private static void ListOptionsAction()
+    {
+        var settings = Settings.LoadSettingsFile();
+        Console.WriteLine("Settings:");
+        ListSettings(settings);
+    }
+
+    private void SetOptionsAction(ParseResult parseResult)
+    {
+        var optionValues = SetOptions.ToDictionary(
+            option => option.HelpName,
+            parseResult.GetValue
+        );
+
+        if (optionValues.Values.All(string.IsNullOrWhiteSpace))
+        {
+            Console.WriteLine("No new settings provided. Too see options, run: ");
+            Console.WriteLine($"{AppDomain.CurrentDomain.FriendlyName} {Name} {parseResult.CommandResult.Command.Name} -h");
+            return;
+        }
+
+        var currentSettings = Settings.LoadSettingsFile();
+        SettingsProperties.ForEach(property =>
+        {
+            var value = optionValues[property.Name];
+            if (!string.IsNullOrWhiteSpace(value))
+                property.SetValue(currentSettings, value);
+        });
+        currentSettings.Save();
+
+        Console.WriteLine("Updated settings:");
+        ListSettings(currentSettings);
+    }
+
     private static void ListSettings(Settings settings) => SettingsProperties.ForEach(property =>
         Console.WriteLine($"  {property.Name}: '{property.GetValue(settings)}'"));
+
 
     [GeneratedRegex(@"([a-z0-9])([A-Z])")]
     private static partial Regex PascalCaseRegex();
