8516742c94b8562e8a734153dc9b6af7df794692
Robin Claesson <57729281+RobinClaesson@users.noreply.github.com>
2025-12-25 13:47:31 +0100

    Include wiki link in throttle exception

--- AdventOfCode.InputHandler/Exceptions/AdventOfCodeThrottleException.cs
+++ AdventOfCode.InputHandler/Exceptions/AdventOfCodeThrottleException.cs
@@ -4,6 +4,7 @@ public class AdventOfCodeThrottleException(RequestInfo? lastRequest)
     : AdventOfCodeException(
         $"""
          Requests to Advent of Code can only be made every {AdventOfCodeClient.RequestThrottleSeconds} seconds.
+         See: https://old.reddit.com/r/adventofcode/wiki/faqs/automation#wiki_throttle_your_outbound_requests
          Next allowed request at UTC {lastRequest?.NextRequestAllowedAt ?? DateTime.UtcNow.AddSeconds(AdventOfCodeClient.RequestThrottleSeconds)}.
          Use {nameof(AdventOfCodeClient)}.{nameof(AdventOfCodeClient.CanMakeRequest)}() to know if request is allowed.
          """)
