a31797571b88a2ccfe2f9a409a57d30cf10bcc84
Robin Claesson <57729281+RobinClaesson@users.noreply.github.com>
2026-08-23 20:30:50 +0200

    Add IntCodeComputer tests

---
+++ AdventOfCode.Tests/SolutionTests/Year2019/IntCodeComputerTests.cs
@@ -0,0 +1,29 @@
+﻿using AdventOfCode.Solutions.Year2019;
+
+namespace AdventOfCode.Tests.SolutionTests.Year2019;
+
+public class IntCodeComputerTests
+{
+    [Theory]
+    [CombinatorialData]
+    public void Day2_RunOutputsPositionZero(
+        [CombinatorialMemberData(nameof(Day2Examples))]
+        IntCodeComputerTestCase<int> testCase)
+    {
+        var computer = new IntCodeComputer(testCase.Program);
+
+        var result = computer.Run();
+
+        Assert.Equal(testCase.Expected, result);
+        Assert.Equal(testCase.Expected, computer[0]);
+    }
+
+    public record IntCodeComputerTestCase<T>(List<int> Program, T Expected);
+
+    public static List<IntCodeComputerTestCase<int>> Day2Examples() =>
+    [
+        new([1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50], 3500),
+        new([1, 0, 0, 0, 99], 2),
+        new([1, 1, 1, 4, 99, 5, 6, 0, 99], 30),
+    ];
+}
